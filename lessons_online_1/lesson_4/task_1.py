5b57cc585634ef61ca11a205b4b2d1cdf86b32d2
Alexander Mangazeev <alex3287@bk.ru>
2022-02-06 16:43:17 +0300

    correct

--- lessons_online_1/lesson_4/task_1.py
+++ lessons_online_1/lesson_4/task_1.py
@@ -5,6 +5,7 @@ max_sum = -20001
 for i in range(len(A)-1):
     if A[i] % 3 == 0 or A[i+1] % 3 == 0:
         count += 1
-        if A[i] + A[i+1] > max_sum:
+        if A[i] + A[i + 1] > max_sum:
             max_sum = A[i] + A[i + 1]
 print(count, max_sum)
+
