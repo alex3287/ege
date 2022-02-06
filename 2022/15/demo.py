d4035ba7272401a4011540eb018bc824558bf4e9
Alexander Mangazeev <alex3287@bk.ru>
2022-02-06 19:18:47 +0300

    demo 22

---
+++ 2022/15/demo.py
@@ -0,0 +1,16 @@
+def f(x, a1, a2):
+    return ((17 <= x <=58) <= ((not(29 <= x <= 80) and \
+                                not(a1 <= x < a2)) <= (not(17 <= x <= 58))))
+
+maxi = 10000
+for a1 in range(-100, 100):
+    for a2 in range(-100, 100):
+        flag = 1
+        for x in range(-100, 100):
+            if f(x, a1, a2) == 0:
+                flag = 0
+                break
+        if flag:
+            if a2 - a1 < maxi:
+                maxi = a2 - a1
+print(maxi)
