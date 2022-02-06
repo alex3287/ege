7bad8c3ed0c8143d74ce4cc8c08a54ca97c25303
Alexander Mangazeev <alex3287@bk.ru>
2022-02-06 16:19:43 +0300

    correct

--- 2022/17/demo.py
+++ 2022/17/demo.py
@@ -1,15 +1,13 @@
 A = [int(i) for i in open('input.txt')]
-# with open('input.txt') as fin:
-#     A = [int(i) for i in fin.readlines()]
-# print(A)
+print(A)
 
 count = 0
 max_sum = -100000
 
-for i in range(len(A) - 1):
+for i in range(len(A)-1):
     if A[i] % 3 == 0 or A[i+1] % 3 == 0:
         count += 1
         if A[i] + A[i+1] > max_sum:
-            max_sum = A[i] + A[i+1]
+            max_sum = A[i] + A[i + 1]
 
 print(count, max_sum)
